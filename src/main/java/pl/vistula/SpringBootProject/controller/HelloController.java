76c87d3567449b13e99abc1eab735ea36bcf8a56
Smlv05 <ersultan270502@gmail.com>
2025-05-13 20:49:31 +0200

    Adding element returning View

--- src/main/java/pl/vistula/SpringBootProject/controller/HelloController.java
+++ src/main/java/pl/vistula/SpringBootProject/controller/HelloController.java
@@ -1,4 +1,22 @@
 package pl.vistula.SpringBootProject.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello Vistula, in my first Spring Controller";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    //http://localhost:8080/greeting?name=Vistula
 }
